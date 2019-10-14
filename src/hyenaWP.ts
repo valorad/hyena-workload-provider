9e3ce5f7a31042eb110d21eca569cec772f7f5c6
valorad <valorad@outlook.com>
2019-10-14 10:40:58 -0400

    init project structure

---
+++ src/hyenaWP.ts
@@ -0,0 +1,41 @@
+import { resolve } from 'path';
+
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as logger from 'koa-logger';
+import * as serve from 'koa-static';
+import * as bodyParser from 'koa-bodyparser';
+import * as cors from "@koa/cors";
+
+import { ConfigLoader } from './utils/configLoader';
+
+const app = new Koa();
+
+let config = new ConfigLoader("hyenaWP.json").config();
+
+// handling config
+if (config) {
+  let port: number = config.koa.port;
+
+  const router = new Router();
+  
+  app.use(logger());
+  app.use(cors());
+  app.use(bodyParser());
+  app.use(serve(resolve(__dirname, "./statics")));
+
+  // router.use('/api', api.routes(), api.allowedMethods())
+  // app.use(router.routes())
+  // .use(router.allowedMethods());
+
+  // listen
+  app.listen(port, () => {
+    console.log(`** koa started on port ${port}. **`);
+  });
+
+} else {
+  console.error("Invalid config detected, program shutting down");
+  process.exit(-1);
+}
+
+export default app;
