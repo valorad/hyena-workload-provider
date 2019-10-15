d851aba3071b9819c5c2fd045c57f3ecb75e522f
valorad <valorad@outlook.com>
2019-10-14 20:34:27 -0400

    grpc: model files

---
+++ server-grpc/src/models/workload.interface.ts
@@ -0,0 +1,27 @@
+interface IListOptions {
+  rfwID?: string,
+  category: string,
+  fields?: string[],
+  unitSize?: number,
+  batchID?: number,
+  batchSize?: number,
+}
+
+interface IWorkload {
+  CPUUtilization_Average: number,
+  NetworkIn_Average: number,
+  NetworkOut_Average: number,
+  MemoryUtilization_Average: number,
+  Final_Target: number,
+}
+
+interface IWorkloadService {
+  List: (options: IListOptions) => IWorkload[]
+}
+
+export interface IWorkloadProto {
+  ListOptions: IListOptions,
+  Workload: IWorkload,
+  WorkloadList: IWorkload[],
+  WorkloadService: IWorkloadService,
+}
