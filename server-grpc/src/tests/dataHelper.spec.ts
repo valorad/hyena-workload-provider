3a427588e9d33faf4d4db7527063a2eebc0db3a0
valorad <valorad@outlook.com>
2019-10-18 11:48:11 -0400

    gRPC: correct dh test typings

--- server-grpc/src/tests/dataHelper.spec.ts
+++ server-grpc/src/tests/dataHelper.spec.ts
@@ -1,4 +1,5 @@
 import { DataHelper } from "../dataHelper";
+import Long = require("long");
 
 
 
@@ -30,29 +31,29 @@ export const dataHelperspec = describe("Data Helper spec", () => {
     const mockData = [
       {
         CPUUtilization_Average: 47,
-        NetworkIn_Average: 237680598,
-        NetworkOut_Average: 239043377,
+        NetworkIn_Average: new Long(237680598),
+        NetworkOut_Average: new Long(239043377),
         MemoryUtilization_Average: 52.91314954,
         Final_Target: 47.77583879
       },
       {
         CPUUtilization_Average: 46,
-        NetworkIn_Average: 259965162,
-        NetworkOut_Average: 258764450,
+        NetworkIn_Average: new Long(259965162),
+        NetworkOut_Average: new Long(258764450),
         MemoryUtilization_Average: 52.78844261,
         Final_Target: 47.15173944
       },
       {
         CPUUtilization_Average: 42,
-        NetworkIn_Average: 233246999,
-        NetworkOut_Average: 218742552,
+        NetworkIn_Average: new Long(233246999),
+        NetworkOut_Average: new Long(218742552),
         MemoryUtilization_Average: 46.68909772,
         Final_Target: 42.35888502
       },
       {
         CPUUtilization_Average: 33,
-        NetworkIn_Average: 194803090,
-        NetworkOut_Average: 185055560,
+        NetworkIn_Average: new Long(194803090),
+        NetworkOut_Average: new Long(185055560),
         MemoryUtilization_Average: 41.72902283,
         Final_Target: 35.66691474
       }
@@ -75,22 +76,22 @@ export const dataHelperspec = describe("Data Helper spec", () => {
     const mockData = [
       {
         CPUUtilization_Average: 33,
-        NetworkIn_Average: 194803090,
-        NetworkOut_Average: 185055560,
+        NetworkIn_Average: new Long(194803090),
+        NetworkOut_Average: new Long(185055560),
         MemoryUtilization_Average: 41.72902283,
         Final_Target: 35.66691474
       },
       {
         CPUUtilization_Average: 31,
-        NetworkIn_Average: 171400566,
-        NetworkOut_Average: 163390599,
+        NetworkIn_Average: new Long(171400566),
+        NetworkOut_Average: new Long(163390599),
         MemoryUtilization_Average: 40.33998435,
         Final_Target: 34.3093731
       },
       {
         CPUUtilization_Average: 37,
-        NetworkIn_Average: 183125327,
-        NetworkOut_Average: 174387148,
+        NetworkIn_Average: new Long(183125327),
+        NetworkOut_Average: new Long(174387148),
         MemoryUtilization_Average: 40.53621278,
         Final_Target: 36.91196676
       }
@@ -118,36 +119,36 @@ export const dataHelperspec = describe("Data Helper spec", () => {
     const mockData = [
       {
         CPUUtilization_Average: 33,
-        NetworkIn_Average: 194803090,
-        NetworkOut_Average: 185055560,
+        NetworkIn_Average: new Long(194803090),
+        NetworkOut_Average: new Long(185055560),
         MemoryUtilization_Average: 41.72902283,
         Final_Target: 35.66691474
       },
       {
         CPUUtilization_Average: 33,
-        NetworkIn_Average: 194803090,
-        NetworkOut_Average: 185055560,
+        NetworkIn_Average: new Long(194803090),
+        NetworkOut_Average: new Long(185055560),
         MemoryUtilization_Average: 41.72902283,
         Final_Target: 35.66691474
       },
       {
         CPUUtilization_Average: 40,
-        NetworkIn_Average: 209125328,
-        NetworkOut_Average: 204387147,
+        NetworkIn_Average: new Long(209125328),
+        NetworkOut_Average: new Long(204387147),
         MemoryUtilization_Average: 41.53621278,
         Final_Target: 40.91196676
       },
       {
         CPUUtilization_Average: 46,
-        NetworkIn_Average: 259965162,
-        NetworkOut_Average: 258764450,
+        NetworkIn_Average: new Long(259965162),
+        NetworkOut_Average: new Long(258764450),
         MemoryUtilization_Average: 52.78844261,
         Final_Target: 47.15173944
       },
       {
         CPUUtilization_Average: 37,
-        NetworkIn_Average: 183125327,
-        NetworkOut_Average: 174387148,
+        NetworkIn_Average: new Long(183125327),
+        NetworkOut_Average: new Long(174387148),
         MemoryUtilization_Average: 40.53621278,
         Final_Target: 36.91196676
       }
